2161a209ac3e0c75e36b50bc902b41a9290285d7
Aralya Phinith <aphinith84@gmail.com>
2016-08-22 13:56:39 -0700

    add react class and comment out for later use if needed

--- src/entry.js
+++ src/entry.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { render } from 'react-dom'
 import App from './components/app'
 import '.styles/app.scss'
 
+
 render(<App/>, document.getElementById('main'))
+
+// class HelloWorld extends Component {
+
+//   render() {
+
+//     return (
+//       <div>
+//         Hello World!
+//       </div>
+//     )
+//   }
+// }
