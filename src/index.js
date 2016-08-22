e167224b7bbbf6b634946e879c245c653cf76068
Aralya Phinith <aphinith84@gmail.com>
2016-08-22 15:11:35 -0700

    still troubleshooting errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import App from './components/app'
-import '.styles/app.scss'
+import './styles/app.scss'
 
 
 render(<App/>, document.getElementById('main'))
